6bb948d24623e534b74f58d7a1c1e29c2ff982ce
jeffniv <jeffniv@yahoo.com>
2022-08-15 11:53:56 -0600

    initial commit

---
+++ bisect.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def bisect(f, a, b, delta=1e-3, i_lim=1e3):
+    if f(a)*f(b) > 0:
+        print('Invalid interval.')
+        return np.nan
+    i = 0
+    while ((np.abs(a-b) > delta) and (i < i_lim)):
+        c = (a+b)/2
+        if f(a)*f(c) > 0:
+            a = c
+        else:
+            b = c
+        i = i+1
+    print(i, 'iterations.')
+    print('delta =', a-b, '.')
+    return a
