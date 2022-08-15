373088299a6c83b860bc18a32a7322d6d6768701
jeffniv <jeffniv@yahoo.com>
2022-08-15 15:52:47 -0600

    encapsulate main program

--- testExample.py
+++ testExample.py
@@ -6,13 +6,18 @@ from bisectExample import bisect
 from classExample import Bisector
 import numpy as np
 
-f = lambda x: x**2
-g = lambda x: f(x)-2
-soln = bisect(g, 0, 2)
-print(f'Solution is {soln}')
+def main():
+    f = lambda x: x**2
+    g = lambda x: f(x)-2
+    soln = bisect(g, 0, 2)
+    print(f'Solution is {soln}')
 
-testobj = Bisector()
-testobj.setFxn(f)
-testobj.setTarget(2)
-testobj.setBounds(0, 10)
-testobj.bisect()
+    testobj = Bisector()
+    testobj.setFxn(f)
+    testobj.setTarget(2)
+    testobj.setBounds(0, 10)
+    testobj.bisect()
+    return
+    
+if __name__ == '__main__':
+    main()
